1bb4295c79cd8f50efa3365a37c69df443789879
Ankit Jha <71430568+ankit6686510@users.noreply.github.com>
2024-01-17 08:55:54 +0000

    conversion of datatypes

---
+++ basic/03_conversionOperation.js
@@ -0,0 +1,25 @@
+let score = "25abc"
+
+//console.log(typeof score);
+//console.log(typeof(score));//scope as a method mtlb writing in parenthesis
+
+let valueInNumber =  Number(score)//convert string in number
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);//NaN not a number
+
+//"33"==>nummber
+//"33abc"==> NaN
+//true ==>1 false==>0
+
+let isLoggedIn ="ankit" ;
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+//note 1 ==>true , 0 ==> false
+//""==>false
+//"ankit"==>true
+
+let someNumber = 22
+let StringNumber = String(someNumber)//number has changed into string
+console.log(StringNumber)
+console.log(typeof StringNumber)
