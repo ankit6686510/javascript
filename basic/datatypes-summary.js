dc4c7587d4dbbcf041f41b6ee51b0e3d6f83c258
Ankit Jha <71430568+ankit6686510@users.noreply.github.com>
2024-01-18 07:58:20 +0000

    datatypesSummary

---
+++ basic/datatypes-summary.js
@@ -0,0 +1,35 @@
+//primitive datatypes
+//7 types:string,number,boolean,null,undefined,symbol,bigInt
+
+//Non primitive or Refernce type
+//array,objects,functions
+
+//is js in dynamiaccly or statically?  
+//ans 
+// const score = 100
+// const scorevalue = 100.3
+
+// const isLogegdIn = false
+// const outsideTemp = null
+// let userEmail;
+// const id =  symbol('123')
+// const anotherId = symbol('123')
+// // console.log(id===anotherId);
+
+// const bigNumber = 34544848545488989n 
+
+// //arrays example 
+const heroes = ["shaktiman","nagraaj","doga"];
+let myobj ={ //{iske andr jo bhi hai sab ovject hai}
+    name:"ankit",
+    age :22,
+}
+
+//funstion 
+const myFunction = function(){
+    // console.log("hello world");
+
+}
+console.log(typeof heroes);
+
+//assignment:read about datatypes
